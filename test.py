89451c3d0dc4694b252674efc46091d461826cc8
Atul Sinha <sinhaa297@gmail.com>
2018-02-07 16:52:34 +0530

    test.py added for calculating accuracy

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
 import numpy as np 
-import 
-def main():
+
+def accuracy(lis_of_tuples):
+	n = len(lis_of_tuples)
+	ctr = 0
+	for i in lis_of_tuples:
+		if i[0] == i[1]:
+			ctr += 1
+
+	accuracy_percentage = ctr/(1.0 * n)
+	accuracy_percentage *= 100
+	return accuracy_percentage
+	
