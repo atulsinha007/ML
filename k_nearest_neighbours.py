f95904df61f37ec139f1c12ac364b4c6d0c78df8
Atul Sinha <sinhaa297@gmail.com>
2018-02-07 16:38:19 +0530

    dataset and load_data added

---
+++ k_nearest_neighbours.py
@@ -0,0 +1,14 @@
+def point:
+
+	def __init__(self, dimension, cordinates, typ):
+		self.dim = dimension
+		self.cordi = cordinates
+		self.type = typ
+
+
+
+def main():
+
+
+if __name__ == "__main__":
+	main()
